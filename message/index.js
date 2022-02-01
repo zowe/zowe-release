ef5990ea8c3f412ffee9a00147df16c3bd48c545
Tom Zhang <tom.yczhang@gmail.com>
2022-02-01 18:00:54 -0500

    add new message step
    Signed-off-by: Tom Zhang <tom.yczhang@gmail.com>

---
+++ message/index.js
@@ -0,0 +1,65 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2022
+ */
+
+const core = require('@actions/core')
+const { utils } = require('zowe-common')
+const fs = require('fs')
+
+// Defaults
+const urlPrefix = 'https://zowe.jfrog.io/zowe/'
+
+// Gets inputs
+var releaseArtifactDownloadFile = core.getInput('release-artifact-download-file')
+var releaseVersion = core.getInput('release-version')
+var buildNum = core.getInput('build-num')
+
+//mandatory check
+utils.mandatoryInputCheck(releaseArtifactDownloadFile, 'release-artifact-download-file')
+utils.mandatoryInputCheck(releaseVersion, 'release-version')
+utils.mandatoryInputCheck(buildNum, 'build-num')
+
+// init
+var releaseArtifactJsonObject = JSON.parse(fs.readFileSync(releaseArtifactDownloadFile))
+var message = []
+message.push(`*************************************************************************************************
+Build ${buildNum} is promoted as Zowe ${releaseVersion}, you can download from below:
+`)
+
+releaseArtifactJsonObject.files.forEach(function(obj) { 
+    var pattern = obj.pattern
+    console.log(pattern)
+    if (pattern.contains('zowe') && pattern.endWith('.pax')) {
+        message.push("Convenience Pax: %s%s", urlPrefix, pattern)
+    }
+    else if(pattern.contains('zowe-smpe') && pattern.endWith('zip')) {
+        message.push("SMPE: %s%s",urlPrefix, pattern )
+    }
+    else if(pattern.contains('server-bundle.amd64') && pattern.endWith('tar')) {
+        message.push("Technical preview docker amd64 image: %s%s",urlPrefix, pattern)
+    }
+    else if(pattern.contains('server-bundle.s390x') && pattern.endWith('tar')) {
+        message.push("Technical preview docker s390x image: %s%s",urlPrefix, pattern)
+    }
+    else if(pattern.contains('zowe-containerization') && pattern.endWith('zip')) {
+        message.push("Containerization: %s%s",urlPrefix, pattern)
+    }
+    else if(pattern.contains('zowe-cli-package') && pattern.endWith('zip')) {
+        message.push("CLI Core Package: %s%s",urlPrefix, pattern)
+    }
+    else if(pattern.contains('zowe-cli-plugins') && pattern.endWith('zip')) {
+        message.push("CLI Plugins Package: %s%s",urlPrefix, pattern)
+    }
+    else {
+        throw new Error('something went wrong')
+    }
+})
+message.push('*************************************************************************************************')
+console.log(message.join('\n'))
+
