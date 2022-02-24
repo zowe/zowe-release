c4de525824e8f44a5e69d460ae0cfb5bd1c96a28
Tom Zhang <tom.yczhang@gmail.com>
2022-02-24 17:31:20 -0500

    More on nightly pipeline
    Signed-off-by: Tom Zhang <tom.yczhang@gmail.com>

--- message/dist/index.js
+++ message/dist/index.js
@@ -6607,6 +6607,7 @@ utils.mandatoryInputCheck(buildNum, 'build-num')
 // init
 var releaseArtifactJsonObject = JSON.parse(fs.readFileSync(releaseArtifactDownloadFile))
 var message = []
+var slackMessage = []
 message.push(`*************************************************************************************************
 Build ${buildNum} is promoted as Zowe ${releaseVersion}, you can download from below:
 `)
@@ -6615,29 +6616,38 @@ releaseArtifactJsonObject.files.forEach(function(obj) {
     var pattern = obj.pattern
     if (pattern.includes('zowe') && pattern.endsWith('.pax')) {
         message.push(`Convenience Pax: ${urlPrefix}${pattern}`)
+        slackMessage.push(`Convenience Pax: ${urlPrefix}${pattern}`)
     }
     else if(pattern.includes('zowe-smpe') && pattern.endsWith('zip')) {
         message.push(`SMPE: ${urlPrefix}${pattern}`)
+        slackMessage.push(`SMPE: ${urlPrefix}${pattern}`)
     }
     else if(pattern.includes('server-bundle.amd64') && pattern.endsWith('tar')) {
         message.push(`Technical preview docker amd64 image: ${urlPrefix}${pattern}`)
+        slackMessage.push(`Technical preview docker amd64 image: ${urlPrefix}${pattern}`)
     }
     else if(pattern.includes('server-bundle.s390x') && pattern.endsWith('tar')) {
         message.push(`Technical preview docker s390x image: ${urlPrefix}${pattern}`)
+        slackMessage.push(`Technical preview docker s390x image: ${urlPrefix}${pattern}`)
     }
     else if(pattern.includes('zowe-containerization') && pattern.endsWith('zip')) {
         message.push(`Containerization: ${urlPrefix}${pattern}`)
+        slackMessage.push(`Containerization: ${urlPrefix}${pattern}`)
     }
     else if(pattern.includes('zowe-cli-package') && pattern.endsWith('zip')) {
         message.push(`CLI Core Package: ${urlPrefix}${pattern}`)
+        slackMessage.push(`CLI Core Package: ${urlPrefix}${pattern}`)
     }
     else if(pattern.includes('zowe-cli-plugins') && pattern.endsWith('zip')) {
         message.push(`CLI Plugins Package: ${urlPrefix}${pattern}`)
+        slackMessage.push(`CLI Plugins Package: ${urlPrefix}${pattern}`)
     }
 })
 message.push(`
 *************************************************************************************************`)
 console.log(message.join('\n'))
+core.setOutput('slack-message',slackMessage.join('\n'))
+
 
 
 })();
